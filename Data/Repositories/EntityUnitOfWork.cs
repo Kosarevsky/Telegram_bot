7b1b3195949768d9eec346bad8145011c7f1d3b6
Dzmitry Kasareuski <dzmitrykasareuski@gmail.com>
2024-10-27 13:53:34 +0100

    Add IUnitOfWork

---
+++ Data/Repositories/EntityUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Data.Context;
+using Data.Interfaces;
+
+
+namespace Data.Repositories
+{
+    public class EntityUnitOfWork : IUnitOfWork
+    {
+        private readonly NotifyKPContext _context;
+        private OperationRepository _operationRepository;
+        public IOperationRepository Operations => 
+            _operationRepository ?? (_operationRepository = new OperationRepository(_context));
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
